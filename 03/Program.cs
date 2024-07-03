d1460c111880952e0a46a4ca797152288655411f
Adriano Teixeira <adrianoteixeira1223@gmail.com>
2024-07-03 19:15:03 -0300

    novo

---
+++ 03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main()
+    {
+        bool opcoes = true;
+        while (opcoes)
+        {
+            Console.WriteLine("Fale um número: ");
+            int A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Fale um número: ");
+            int B = int.Parse(Console.ReadLine());
+
+            
+        }
+    }
+}
